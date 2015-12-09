7b83810df3f98656609d15807be013803c87107d
jonBarcus <barcus.j@gmail.com>
2015-12-08 20:15:44 -0500

    double checked everything...doesn't seem to be a problem on my end

--- app.rb
+++ app.rb
@@ -6,12 +6,12 @@ post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
 
   action = message.split('_').map {|c| c.strip.downcase }
-  repo_url = 'https://api.github.com/repos/jonBarcus/SoIaF_GM_Tools'
+  repo_url = "https://api.github.com/repos/jonBarcus/SoIaF_GM_Tools"
 
   case action
     when 'issues'
       resp = HTTParty.get(repo_url)
-      # resp = JSON.parse resp.body
+      resp = JSON.parse resp.body
       respond_message "There are #{resp['open_issues_count']} open issues on SoIaF_GM_Tools"
     end
   end
