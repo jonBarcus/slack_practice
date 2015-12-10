de6a1da76a37332a2808d74c06f443178dcc2e05
jonBarcus <barcus.j@gmail.com>
2015-12-10 13:34:03 -0500

    now, maybe?

--- app.rb
+++ app.rb
@@ -3,15 +3,17 @@ require 'httparty'
 require 'json'
 
 post '/gateway' do
-  action = params[:text].gsub(params[:trigger_word], '').strip
 
+  message = params[:text].gsub(params[:trigger_word], '').strip
+
+  action, repo = message.split('_').map {|c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/jonBarcus/SoIaF_GM_Tools"
 
   case action
     when 'issues'
       resp = HTTParty.get(repo_url)
       resp = JSON.parse resp.body
-      respond_message "There are #{resp['open_issues_count']} open issues on SoIaF_GM_Tools"
+      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
   end
 end
 
