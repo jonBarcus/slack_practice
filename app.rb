efbdca914262ee8628471386e84abf1a8ad023b3
jonBarcus <barcus.j@gmail.com>
2015-12-10 10:43:20 -0500

    added 'do'

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-post '/gateway'
+post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
 
   action, repo = message.split('_').map {|c| c.strip.downcase }
