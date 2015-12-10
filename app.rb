53fbb7d4b82988afaee45f6fe96a4db6611d0573
jonBarcus <barcus.j@gmail.com>
2015-12-09 19:01:21 -0500

    rewrote the whole...thing...

--- app.rb
+++ app.rb
@@ -2,21 +2,21 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-post '/gateway' do
+post '/gateway'
   message = params[:text].gsub(params[:trigger_word], '').strip
 
-  action = message.split('_').map {|c| c.strip.downcase }
-  repo_url = "https://api.github.com/repos/jonBarcus/SoIaF_GM_Tools"
+  action, repo = message.split('_').map {|c| c.strip.downcase }
+  repo_url = "https://api.github.com/repos/#{repo}"
 
   case action
     when 'issues'
       resp = HTTParty.get(repo_url)
       resp = JSON.parse resp.body
-      respond_message "There are #{resp['open_issues_count']} open issues on SoIaF_GM_Tools"
-    end
+      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
   end
+end
 
-  def respond_message message
-    content_type :json
-    {:text => message}.to_json
-  end
+def respond_message message
+  content_type :json
+  {:text => message}.to_json
+end
